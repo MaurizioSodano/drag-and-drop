eab2ef341b6a396da10e97a002864c3627cc77be
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-07-28 19:39:31 +0200

    gather user input

--- src/app.ts
+++ src/app.ts
@@ -42,11 +42,37 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput():[string, string, number]|void {
+    const enteredTitle=this.titleInputElement.value;
+    const enteredDescription=this.descriptionInputElement.value;
+    const enteredPeople=this.peopleInputElement.value;
+
+    if(enteredPeople.trim().length===0 ||
+    enteredTitle.trim().length===0 ||
+    enteredDescription.trim().length===0){
+      alert("Invalid input");
+      return;
+    }else{
+      this.clearInputs()
+      return [enteredTitle,enteredDescription,+enteredPeople];
+    }
+  }
+
+private clearInputs(){
+  this.titleInputElement.value="";
+  this.descriptionInputElement.value="";
+  this.peopleInputElement.value="";
+}
+
   @Autobind
   private submitHandler(event:Event){
-    event.preventDefault();
-    console.log(this.titleInputElement.value)
-
+    event.preventDefault(); 
+    const userInput=this.gatherUserInput();
+    if(Array.isArray(userInput)){
+      const [title,description,people]=userInput;
+      console.log(title,description,people);
+      
+    }
   }
   private configure(){
     this.element.addEventListener("submit",this.submitHandler)
